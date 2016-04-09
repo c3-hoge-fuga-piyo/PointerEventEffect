992229e0c4dfae7a833956147684c287f663d0ff
Shimoda Takao <c3.hoge.fuga.piyo@gmail.com>
2016-04-10 03:20:47 +0900

    Initial commit.

---
+++ Assets/Scripts/PointerEventEffect.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+using System.Collections.Generic;
+
+[RequireComponent(typeof(Camera))]
+public sealed class PointerEventEffect : BaseRaycaster
+{
+    #region Unity Inspectors.
+    [SerializeField]
+    ParticleSystem tap;
+
+    [SerializeField]
+    ParticleSystem swipe;
+    #endregion
+
+    #region Unity Messages.
+    protected override void Awake()
+    {
+        base.Awake();
+
+        this.cachedCamera = this.GetComponent<Camera>();
+    }
+    #endregion
+
+    bool lastState = false;
+
+    Camera cachedCamera;
+
+    #region BaseRaycaster
+    public override Camera eventCamera { get { return this.cachedCamera; } }
+
+    public override void Raycast(
+        PointerEventData eventData,
+        List<RaycastResult> resultAppendList)
+    {
+        var e = eventData.eligibleForClick;
+        try
+        {
+            if (e)
+            {
+                var ep = eventData.position;
+                var p = this.eventCamera.ScreenToWorldPoint(
+                        new Vector3(
+                            ep.x,
+                            ep.y,
+                            -this.eventCamera.transform.position.z));
+
+                this.swipe.transform.position = p;
+                if (!this.lastState)
+                {
+                    // Down
+                    this.swipe.Play();
+
+                    this.tap.transform.position = p;
+                    this.tap.Play();
+                }
+
+                return;
+            }
+
+            if (this.lastState)
+            {
+                // Up
+                this.swipe.Stop();
+            }
+        }
+        finally
+        {
+            this.lastState = e;
+        }
+    }
+    #endregion
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+Pointer event effect
+
+[![CC0](http://img.shields.io/badge/license-CC0-blue.svg?style=flat-square)](COPYING.txt)
+
+マウスやタッチの入力に応じたエフェクトを再生するサンプル（[WebGL版](http://c3-hoge-fuga-piyo.github.io/PointerEventEffect/)）
+
+![Screenshot](image.png)
